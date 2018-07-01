629ba8a0479c47fb65431d68f9b69315a4c3d643
lppedd <lp.edoardo@gmail.com>
2018-07-01 21:22:06 +0200

    Wildfly compatibility

--- edoardo-test-war/src/main/java/com/edoardo/test/war/rest/UserRestService.java
+++ edoardo-test-war/src/main/java/com/edoardo/test/war/rest/UserRestService.java
@@ -6,6 +6,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 
@@ -22,6 +24,7 @@ public class UserRestService {
 	private UserService userService;
 
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<User> allUsers() {
 		logger.info("Invoking the service to retrieve the users list");
 		return userService.findAllUsers();
